68983f568e5cf85387f89c8dd862a67a949c6958
Ricardo Rocha <ricardorrf@gmail.com>
2021-05-29 16:55:49 -0300

    Studying rest

---
+++ src/main/java/com/github/rrochaf/rest/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.github.rrochaf.rest;
+
+public class HelloWorldBean {
+
+	private String message;
+	
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
+
---
+++ src/main/java/com/github/rrochaf/rest/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.github.rrochaf.rest;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200/")
+@RestController
+public class HelloWorldController {
+
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		//throw new RuntimeException("Some Error has happened!");
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s !!!", name)) ;
+	}
+}
